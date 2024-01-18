f6ced99868f4c2928b97763415afae2f1a373bf7
Michael Walker <pythonk@email.phoenix.edu>
2024-01-18 18:01:16 -0500

    Section 17 - Game Play UI
    Added another Control node to the UI scene to manage three different screens, Game Over, Game Finish and Game Pause. Moved the GameUI script from the first Control node to the root UI node and adjusted the script to accommodate the new node structure. Obviously, we'll be adding to this script to control the features of the added Control node.

---
+++ Game/Scripts/GameManager.gd
@@ -0,0 +1,23 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_button_restart_button_up():
+	print("restart button is pressed")
+
+
+func _on_button_main_menu_button_up():
+	print("main menu button is pressed")
+
+
+func _on_button_resume_button_up():
+	print("resume button is pressed")
--- Game/Scripts/GameplayUI_Manager.gd
+++ Game/Scripts/GameplayUI_Manager.gd
@@ -1,15 +1,38 @@
-extends Control
+class_name GameUIManager
+extends Node3D
 
-@onready var coinLabel: Label = $HBoxContainer_Coin/Label_Coin
-@onready var healthBar: ProgressBar = $HBoxContainer_Health/ProgressBar
+@onready var coinLabel: Label = $Control_GameplayUI/HBoxContainer_Coin/Label_Coin
+@onready var healthBar: ProgressBar = $Control_GameplayUI/HBoxContainer_Health/ProgressBar
+@onready var blackColorRect: ColorRect = $Control_Game/ColorRect
+@onready var gamePause: Container = $Control_Game/GamePause
+@onready var gameFinish: Container = $Control_Game/GameFinish
+@onready var gameOver: Container = $Control_Game/GameOver
 
 @export var player: Node3D
+
+func togglePauseUI(toggle: bool):
+	blackColorRect.visible = toggle
+	gamePause.visible = toggle
+	
+
+func toggleGameOverUI(toggle: bool):
+	blackColorRect.visible = toggle
+	gameOver.visible = toggle
+
+
+func toggleGameFinish(toggle: bool):
+	blackColorRect.visible = toggle
+	gameFinish.visible = toggle
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	player.coinNumberUpdated.connect(updateCoinLabel)
 	player.playerHealthUpdated.connect(updateHealthBar)
 	updateHealthBar(player.currentHealth, player.maxHealth)
+	togglePauseUI(false)
+	toggleGameOverUI(false)
+	toggleGameFinish(false)
 
 
 func updateCoinLabel(newValue: int):
